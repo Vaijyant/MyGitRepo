71418e643faa223eda1f24f759d2d5c51b5ef8d4
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2019-01-21 12:25:55 -0500

    Init feature/VT-feature

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,4 +4,9 @@ clas HelloWorld {
         System.out.println("This line is in develop branch");
         System.out.println("Change 2 in develop branch")
     }
+
+    public static boolean isOdd(int num){
+        //A function in VT-feature
+        return num%2 == 1 ? true : false; 
+    }
 }
